aecb100487f642b0327368225b58e93f18de3544
Benjamin Bolte <ben@bolte.cc>
2024-06-21 12:00:27 -0700

    boilerplate

---
+++ setup.py
@@ -0,0 +1,39 @@
+# mypy: disable-error-code="import-untyped"
+#!/usr/bin/env python
+"""Setup script for the project."""
+
+import re
+
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description: str = f.read()
+
+
+with open("stompy_live/requirements.txt", "r", encoding="utf-8") as f:
+    requirements: list[str] = f.read().splitlines()
+
+
+with open("stompy_live/requirements-dev.txt", "r", encoding="utf-8") as f:
+    requirements_dev: list[str] = f.read().splitlines()
+
+
+with open("stompy_live/__init__.py", "r", encoding="utf-8") as fh:
+    version_re = re.search(r"^__version__ = \"([^\"]*)\"", fh.read(), re.MULTILINE)
+assert version_re is not None, "Could not find version in stompy_live/__init__.py"
+version: str = version_re.group(1)
+
+
+setup(
+    name="stompy_live",
+    version=version,
+    description="The stompy_live project",
+    author="Benjamin Bolte",
+    url="https://github.com/kscalelabs/stompy-live",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.11",
+    install_requires=requirements,
+    tests_require=requirements_dev,
+    extras_require={"dev": requirements_dev},
+)
