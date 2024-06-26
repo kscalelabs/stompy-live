7ed113595bac4d0dc0fb2b2afc0d16dbce882fd4
is2ac2 <wlight@emory.edu>
2024-06-25 19:42:16 -0700

    minor fixes

--- tests/maniskilltest.py
+++ tests/maniskilltest.py
@@ -6,6 +6,7 @@ import mani_skill.envs  # noqa: F401
 env = gym.make(
     "PickCube-v1",  # there are more tasks e.g. "PushCube-v1", "PegInsertionSide-v1", ...
     num_envs=1,
+    robot_uids="panda",
     obs_mode="state",  # there is also "state_dict", "rgbd", ...
     control_mode="pd_ee_delta_pose",  # there is also "pd_joint_delta_pos", ...
     render_mode="human",
@@ -15,6 +16,7 @@ print("Action space", env.action_space)
 
 obs, _ = env.reset(seed=0)  # reset with a seed for determinism
 done = False
+infinite = False
 while not done:
     action = env.action_space.sample()
     obs, reward, terminated, truncated, info = env.step(action)
