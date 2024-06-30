df99470a0fd432d7a485af06170e72bb466395bf
wuallenwu <allenwu.12@outlook.com>
2024-06-29 20:47:26 -0700

    Create skeleton with franka vision
    Wrangling with
    
    	KeyError: 'rgb'
    
    for some reason

--- stompy_live/apis/franka_vision.py
+++ stompy_live/apis/franka_vision.py
@@ -8,9 +8,9 @@ import gymnasium as gym
 import torch
 import uvicorn
 from fastapi import FastAPI, Request, Response
-from mani_skill.utils.wrappers.flatten import FlattenActionSpaceWrapper
+from mani_skill.utils.wrappers.flatten import FlattenActionSpaceWrapper, FlattenRGBDObservationWrapper
 
-from stompy_live.agents.franka_arm import Agent
+from stompy_live.agents.franka_arm_vision import Agent
 
 
 # === Server Interface ===
@@ -19,13 +19,16 @@ class FrankaServer:
         self.model_path = model_path
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-        env_kwargs = dict(obs_mode="rgbd", control_mode="pd_joint_delta_pos", render_mode="human", sim_backend="gpu")
+        env_kwargs = dict(obs_mode="rgbd", control_mode="pd_joint_delta_pos", render_mode="rgb_array", sim_backend="gpu")
         envs = gym.make("PushCube-v1", **env_kwargs)
+        envs = FlattenRGBDObservationWrapper(envs, rgb_only=True)
         if isinstance(envs.action_space, gym.spaces.Dict):
             envs = FlattenActionSpaceWrapper(envs)
         assert isinstance(envs.single_action_space, gym.spaces.Box), "only continuous action space is supported"
 
-        agent = Agent(envs).to(device)
+        next_obs, _ = envs.reset()
+
+        agent = Agent(envs, sample_obs=next_obs).to(device)
         agent.load_state_dict(torch.load(model_path))
         agent.eval()
 
@@ -52,7 +55,7 @@ class FrankaServer:
 @dataclass
 class DeployConfig:
     # fmt: off
-    model_path: Union[str, Path] = "model-rgb.pt"               # Where the model weights are stored
+    model_path: Union[str, Path] = "model.pt"               # Where the model weights are stored
 
     # Server Configuration
     host: str = "0.0.0.0"                                               # Host IP Address
---
+++ stompy_live/clients/franka_vision.py
@@ -0,0 +1,49 @@
+"""Client for franka arms that connects to the franka API (vision model) and sends observations to get actions."""
+
+import argparse
+import io
+
+import gymnasium as gym
+import requests
+import torch
+from mani_skill.utils.wrappers.flatten import FlattenActionSpaceWrapper
+
+# Parse franka API route location from command line arguments
+
+parser = argparse.ArgumentParser(description="Client for Franka API")
+parser.add_argument("--route", type=str, default="http://localhost:8000/act", help="Where the API is hosted")
+args = parser.parse_args()
+
+# Load the model
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+env_kwargs = dict(obs_mode="rgbd", control_mode="pd_joint_delta_pos", render_mode="rgb_array", sim_backend="gpu")
+envs = gym.make("PushCube-v1", **env_kwargs)
+if isinstance(envs.action_space, gym.spaces.Dict):
+    envs = FlattenActionSpaceWrapper(envs)
+assert isinstance(envs.single_action_space, gym.spaces.Box), "only continuous action space is supported"
+
+# Create a session for connection reuse
+session = requests.Session()
+
+while True:
+    obs, info = envs.reset()
+    done = False
+    total_reward = 0
+
+    while not done:
+        # Get action from the model hosted at the API
+        with torch.no_grad():
+            buffer = io.BytesIO()
+            torch.save(obs, buffer)
+            obs_bytes = buffer.getvalue()
+
+            print("Sending post request...")
+            action_bytes = session.post(args.route, data=obs_bytes).content
+            print("Loading action bytes...")
+            action = torch.load(io.BytesIO(action_bytes))
+
+        obs, reward, terminated, truncated, info = envs.step(action)
+
+        done = terminated or truncated
+        total_reward += reward
+        envs.render()
